b7c30336e1f55d2e74076737ef89e5af066da890
John Nenniger <JPNENNIGER@GMAIL.com>
2019-11-08 14:21:48 -0800

    webpack 4 custom config working with new verions of babel and dependencies

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<h1> Sup baby </h1>, document.getElementById('app'))
+
+
+// class App extends Component{
+//     render() {
+//         return (
+//             <div>
+//                 <p>Jack's site babyyy</p>
+//             </div>
+//         )
+//     }
+// }
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [{
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/,
+        }, {
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ],
+        },
+        ]
+    },
+    mode: "development",
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
+
