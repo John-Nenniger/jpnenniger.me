04b0a85e351656674134b38663e8f42edaa41076
John Nenniger <JPNENNIGER@GMAIL.com>
2019-11-28 13:06:08 -0800

    WIP add cleanwebpackplugin, currently deleting my images though

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/app.js',
@@ -12,7 +13,8 @@ module.exports = {
             title: 'JPNenniger.me',
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new CleanWebpackPlugin()
     ],
     module: {
         rules: [{
