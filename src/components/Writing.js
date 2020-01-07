de49291fd41a86d4c03fb64085962c53e9f9c118
John Nenniger <JPNENNIGER@GMAIL.com>
2020-01-06 17:11:19 -0800

    writing improvements

--- src/components/Writing.js
+++ src/components/Writing.js
@@ -15,21 +15,27 @@ const Writing = () => {
                     <p>
                         One of the most important side-projects a developer can work is creating a personal site.  
                         I haven’t gotten to it so far in my career, and it seems like one of those things every developer should have.  
-                        It’ s a great opportunity to use React, to have essentially an online business card, to put myself out into the world, and to  
+                        It’s a great opportunity to use React, to have essentially an online business card, to put myself out into the world, and to  
                         <a className="external_link" href="https://www.taniarascia.com/learn/"> 
                             &nbsp;learn in public
                         </a>.
                     </p>
 
                     <p>
-                        Here are some goals I had before starting the project:
+                        Here are some goals I had before starting the project (marked with a bullet):
                     </p>
 
-                        <ul>Get something simple and functional up relatively quickly, and expand its functionality later.</ul>
+                    <ul>
+                        <div className="writing__bullet">
+                            <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>
+                            Get something simple and functional up relatively quickly, and expand its functionality later.
+                        </div>
+                    </ul>
 
                     <p>
-                        This is the most important thing I tell Codecore students when it comes to their final projects, so I might as well follow the advice myself.  
-                        Decide on the core functionality of your app, make that deadline modest, and worry about the peripheral features later.  
+                        A version of this principle is the most common piece of advice I tell Codecore students when it comes to their final projects,
+                        so I might as well follow the advice myself.  
+                        I usually tell students to: decide on the core functionality of your app, make that deadline modest, and worry about the peripheral features later. 
                         Which leads me to the list of features for the first iteration of the site:
                     </p>
 
@@ -71,13 +77,13 @@ const Writing = () => {
                         This site can be a nice place to show off some photos without the anxious feelings I get posting things on instagram.
                     </p>
 
-                    <p>
-                        Understand thoroughly all the technologies I’m using, avoid libraries for small tasks.
-                    </p>
+                    <ul className="writing__bullet">
+                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>Understand thoroughly all the technologies I’m using, avoid libraries for small tasks.
+                    </ul>
 
                     <p>
                         I want to take the time to do things manually as much as possible, and actually understand the technologies and techniques I’m using rather than just throwing around libraries for quick fixes. 
-                        Obviously this is the goal with everything I do, but It’s easier to achieve when you control the deadlines and the project as a whole.
+                        Obviously this is the goal with everything I do, but it’s easier to achieve when you control the deadlines and the project as a whole.
                     </p>
 
                     <p>
@@ -86,10 +92,10 @@ const Writing = () => {
                     </p>
 
                     <ul className="writing__bullet">
-                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>Complete the project, put it out into the world,
+                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>Complete the project, make it live,
                     </ul>
                     <ul className="writing__bullet">
-                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>try not to stress.
+                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>accept it's imperfections and use them as learning opportunities.
                     </ul>
 
                     <p>
@@ -97,10 +103,10 @@ const Writing = () => {
                     </p>
                     
                     <ul className="writing__bullet">
-                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>relatively weak web design skill compared to general programming ability,
+                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>relatively weak design skill compared to backend programming ability (backend programming is much more of a strength for me),
                     </ul>
                     <ul className="writing__bullet">
-                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>reluctance to put something imperfect behind my name,
+                        <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>reluctance to put something imperfect behind my name, and out into the world in general,
                     </ul>
                     <ul className="writing__bullet">
                         <FontAwesomeIcon className="writing__bullet_icon" icon="dot-circle"/>and the relative discomfort I have with the web design workflow compared with the workflow for a backend task. 
